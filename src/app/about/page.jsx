a165827b2d5c747254856c9036ec4a7e2e616888
Libx09 <ibrahimfuad729@gmail.com>
2026-06-07 01:32:10 +0600

    nav done

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Title from "@/components/Title";
 import React from "react";
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,37 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return (
     <header className="w-full bg-zinc-400 text-xl tex dark:bg-zinc-900  flex px-4 items-center border-b-2">
-      <h2 className="font-semibold h-16 flex items-center">
+      <Link href={"/"} className="font-semibold h-16 flex items-center">
         (❁´◡`❁) Book Bondhu
-      </h2>
+      </Link>
+      <nav className="ml-auto">
+        <ul className="flex space-x-4">
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/dashboard"}>Dashboard</Link>
+          </li>
+          <li>
+            <Link href={"/login"}>Login</Link>
+          </li>
+          <li>
+            <Link href={"/register"}>Register</Link>
+          </li>
+          <li>
+            <Link href={"/stories"}>Stories</Link>
+          </li>
+          <li>
+            <Link href={"/tutorials"}>Tutorials</Link>
+          </li>
+          <li>
+            <Link href={"/about"}>About</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
