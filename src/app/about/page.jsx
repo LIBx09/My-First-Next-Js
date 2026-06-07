65f4a3e68e5c6136d6e069a75318b4c2d38e7a8b
Libx09 <ibrahimfuad729@gmail.com>
2026-06-07 15:50:14 +0600

    btn a userouter use kore pass set kore page navi korlam

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,16 @@
 "use client";
 import Title from "@/components/Title";
+import Link from "next/link";
 import React from "react";
 
 const About = () => {
   return (
     <div>
       <Title>Welcome to about us page</Title>
+      <nav className="flex  gap-2">
+        <Link href="/about/teams">Teams</Link>
+        <Link href="/about/contact">Contact</Link>
+      </nav>
     </div>
   );
 };
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,37 @@
 "use client";
 
-import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleBtn = () => {
+    const password = prompt("Enter Your Password");
+    if (password === "1234") {
+      alert("Access granted!");
+      console.log(router);
+      router.push("/dashboard");
+    } else {
+      alert("Access denied!");
+    }
+  };
+
   return (
-    <div className="flex flex-col text-2xl font-bold bg-zinc-50 font-sans dark:bg-black">
-      <h4>Hello world!</h4>
-      <h3>Welcome to Book Bondhu!</h3>
+    <div className="w-full min-h-screen flex items-center justify-center bg-base-200">
+      <div className="max-w-md mx-auto flex flex-col items-center text-center">
+        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+        <p className="mb-5">
+          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+          id nisi.
+        </p>
+        <button
+          onClick={handleBtn}
+          className="btn border rounded-2xl p-2 cursor-pointer "
+        >
+          Share Story
+        </button>
+      </div>
     </div>
   );
 }
